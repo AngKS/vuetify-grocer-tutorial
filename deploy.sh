478dfe01f8eb9d1d7a3396da46a097e0b14dd275
AKS <60446729+AngKS@users.noreply.github.com>
2022-08-31 15:25:27 +0800

    bug fix

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,16 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+git init
+
 git add -A
 
 # get current time
 time=$(date +%Y%m%d%H%M%S)
-
 git commit -m "[Deploy] - $time"
 
 # push to repo gh-pages branch
-git push --set-upstream origin gh-pages
+git push -f https://github.com/AngKS/vuetify-grocer-tutorial.git master:gh-pages
 
 # Log success
 echo "Deployed to Github Pages!"
